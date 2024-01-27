74fef35c0c61bb739b1deedb26c1f166e44a10ae
Win10 <Win10@10.0.0.15>
2024-01-27 16:33:31 +0200

    addition check Degree

--- QA1/src/main/java/HW2_QA/App.java
+++ QA1/src/main/java/HW2_QA/App.java
@@ -10,4 +10,14 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    // this function should comparen the number thats we get and 
+    public static String checkDegree(int grade) {
+    	if (grade>=1 || grade<=49) return "F";
+    	else if(grade >=50 && grade <=59) return "E";
+    	else if(grade >= 60 && grade <= 69) return "D";
+    	else if(grade >= 70 && grade <= 79) return "C";
+    	else if(grade >= 80 && grade <= 89) return "B";
+    	else if(grade >= 90 && grade <= 100) return "A";
+    	else return "error";
+    }
 }
