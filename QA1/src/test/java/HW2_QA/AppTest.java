3d16ba29cbf5dd9af6c949a66b9d920c9ec90d7e
Win10 <Win10@10.0.0.15>
2024-01-29 21:38:59 +0200

    C_Step_min() C_Step_minPlus1() C_Step_randomInRange() C_Step_maxMinus1() C_Step_max()

--- QA1/src/test/java/HW2_QA/AppTest.java
+++ QA1/src/test/java/HW2_QA/AppTest.java
@@ -18,5 +18,67 @@ public class AppTest
     {
         assertTrue( true );
     }
-
+    /**
+     * this function is PVA test
+     *  test the min number in the range
+     * the number range of C is 70-79
+     */
+    @Test
+    public void C_Step_min() {
+    	String Expected = "C" ;
+    	String Result=App.checkDegree(70);
+    	assertEquals(Result, Expected);
+    }
+    
+    /**
+     * this function is PVA test
+     *  test the min + 1 number in the range
+     * the number range of C is 70-79
+     */
+    
+    @Test
+    public void C_Step_minPlus1() {
+    	String Expected = "C" ;
+    	String Result=App.checkDegree(71);
+    	assertEquals(Result, Expected);
+    }
+    
+    /**
+     * this function is PVA test
+     *  test the random number in the range
+     * the number range of C is 70-79
+     */
+    
+    @Test
+    public void C_Step_randomInRange() {
+    	String Expected = "C" ;
+    	String Result=App.checkDegree(75);
+    	assertEquals(Result, Expected);
+    }
+    
+    /**
+     * this function is PVA test
+     *  test the max - 1 number in the range
+     * the number range of C is 70-79
+     */
+    
+    @Test
+    public void C_Step_maxMinus1() {
+    	String Expected = "C" ;
+    	String Result=App.checkDegree(78);
+    	assertEquals(Result, Expected);
+    }
+    
+    /**
+     * this function is PVA test
+     *  test the max number in the range
+     * the number range of C is 70-79
+     */
+    
+    @Test
+    public void C_Step_max() {
+    	String Expected = "C" ;
+    	String Result=App.checkDegree(79);
+    	assertEquals(Result, Expected);
+    }
 }
